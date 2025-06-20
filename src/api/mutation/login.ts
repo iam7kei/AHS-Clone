003521913127bb4a4cc610ff33f91ce7a0080631
Dyniel Keith Cataluna <iam7kei@gmail.com>
2025-06-21 01:38:26 +0800

    deployment fix

--- src/api/mutation/login.ts
+++ src/api/mutation/login.ts
@@ -9,9 +9,8 @@ export const userLogin = async ({ email, password }: LoginCredentials) => {
   if (!email || !password) {
     throw new Error('Email and password are required');
   }
-
   try {
-    const response = await axios.post('http://localhost:3001/auth/login', {
+    const response = await axios.post('https://ahs-clone-api.onrender.com/auth/login', {
       email,
       password,
     });
